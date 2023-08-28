54bf12a9186e4f3719877de2cfa99b206d3cd0c8
PinskiyGithub <110927886+PinskiyGithub@users.noreply.github.com>
2023-08-28 15:38:04 +0300

    Add files via upload

---
+++ read_chat.php
@@ -0,0 +1,1 @@
+<?phpif (file_exists('chat.txt')) {    echo file_get_contents('chat.txt');}?>
---
+++ save_chat.php
@@ -0,0 +1,1 @@
+<?phpif (isset($_POST['data'])) {    file_put_contents('chat.txt', $_POST['data'], FILE_APPEND);}?>
